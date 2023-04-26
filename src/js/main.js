21ecb6434b5c828a5bd90b8f8565fb8ab2ca5309
Murat Ors <webschool2015@gmail.com>
2023-04-26 19:11:57 +0200

    Added Dropdown menu as a ready-made component

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,21 @@ $('button')
   .on('click', () => {
     $('.w-500').fadeToggle(1000);
   });
+
+$('.wrap').html(
+  `
+      <div class="dropdown">
+        <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">
+          Dropdown button
+        </button>
+        <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
+          <a href="#" class="dropdown-item">Home</a>
+          <a href="#" class="dropdown-item">Forum</a>
+          <a href="#" class="dropdown-item">About</a>
+        </div>
+      </div>
+    </div>     
+    `
+);
+
+$('.dropdown-toggle').dropdown();
