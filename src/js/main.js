7e373b32f61e2f9a6270cc00afc571cc93c02e7c
Murat Ors <webschool2015@gmail.com>
2023-04-26 18:31:21 +0200

    added a fadeToggle animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,15 @@
 import $ from './lib/lib';
 
 $('#first').on('click', () => {
-  $('div').eq(1).fadeOut(1000);
+  $('div').eq(1).fadeToggle(1000);
 });
 
 $('[data-count="second"]').on('click', () => {
-  $('div').eq(2).fadeOut(1000);
+  $('div').eq(2).fadeToggle(1000);
 });
 
 $('button')
   .eq(2)
   .on('click', () => {
-    $('.w-500').fadeOut(1000);
+    $('.w-500').fadeToggle(1000);
   });
